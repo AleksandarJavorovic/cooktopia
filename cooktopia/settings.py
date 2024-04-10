ff0e81166a0de4904ea9819b5631d676ddb6c8b2
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-04-10 15:52:43 +0000

    add checkout view, template

--- cooktopia/settings.py
+++ cooktopia/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'products',
     'shopping_bag',
     'checkout',
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cooktopia.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'shopping_bag.contexts.shopping_bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
