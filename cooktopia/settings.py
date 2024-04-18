f016e38bc336ec3d31673750b8a6f4801dc5880c
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-04-18 11:30:23 +0000

    update secret key and set debug for deployment

--- cooktopia/settings.py
+++ cooktopia/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!lkh6ltjgqcf)-ifp+%$&#losky4qoi$1w)g9adta9dt^a333f'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-aleksandarjav-cooktopia-p8y6r1x9lvb.ws-eu110.gitpod.io', 'cooktopia-3a5b4620860d.herokuapp.com']
 
