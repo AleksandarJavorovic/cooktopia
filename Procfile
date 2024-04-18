8309fd5fb2c536b7a189ceb40f2527fd8e76ddb6
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-04-18 11:02:24 +0000

    update before heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cooktopia.wsgi:application
--- cooktopia/settings.py
+++ cooktopia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,8 @@ SECRET_KEY = 'django-insecure-!lkh6ltjgqcf)-ifp+%$&#losky4qoi$1w)g9adta9dt^a333f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-aleksandarjav-cooktopia-p8y6r1x9lvb.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-aleksandarjav-cooktopia-p8y6r1x9lvb.ws-eu110.gitpod.io',
+                 'https://cooktopia-3a5b4620860d.herokuapp.com/']
 
 
 # Application definition
@@ -121,14 +123,19 @@ WSGI_APPLICATION = 'cooktopia.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
