c83410a96fa039100d8cfae1026f044d712d87b4
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-04-22 13:36:16 +0000

    add wishlist app, set url, add to settings.py

---
+++ wishlist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Wishlist(models.Model): 
+    '''
+    Wishlist model, for users to save their favourite items for later purchase
+    '''
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE,
+                                     null=False, blank=False,
+                                     related_name='user_wishlist',
+                                     default=None)
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.product.name
