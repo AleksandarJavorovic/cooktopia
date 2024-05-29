29052430719109a3cba68f325cfe1e9fd45ee454
Aleksandar Javorovic <aleksandarjavorovic@gmail.com>
2024-05-29 16:42:04 +0000

    add privacy policy page and update footer links

--- home/views.py
+++ home/views.py
@@ -8,3 +8,11 @@ def index(request):
     '''
     
     return render(request, 'home/index.html')
+
+
+
+def privacy_policy(request):
+    '''
+    A view to render the privacy policy page
+    '''
+    return render(request, "home/privacy_policy.html")
